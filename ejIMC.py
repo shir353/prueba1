3fb33c9949c31e5eff8e7002746e8cb3d9c3a319
shir353 <ppaulredford@gmail.com>
2026-08-03 15:24:46 -0400

    este es mi ejercico 11

---
+++ ejIMC.py
@@ -0,0 +1,25 @@
+#Solicitar al usuario ingrese supeso en kilogrammos y su estatura en metros,luego calcular su IMC y mostrar 
+#el IMC utilizando la formula IMC= peso/altura 2
+# mostrar los reultados de IMC
+#Clasificar el estado de peso de la persona segun la OMS
+#RAngo de IMC clasificacion
+# IMC  menos a 18.5 peso bajo
+# IMC 18.5 a 25 peso normal 
+# IMC 25 a 29.9 sobreperso
+# IMC 30 o mas Obesidad
+
+Peso  = float(input("Ingrese su peso en kilogramos:"))
+altura = float(input("Ingrese su estatura en metros:"))
+
+IMC = PESO / (altura**2)
+
+if IMC <18.5:
+    print("BAjo Peso")
+    
+elif IMC <=18.5 and IMC < 25:
+    print("peso Nomral")
+
+elif IMC >= 25.0 and IMC< 30:    
+    print("Sobrepeso")
+    
+else: IMC >=30:    print("Obesidad")
